048878aea091a94ad8870c0c042849cd46cf9a16
Pedro-dev-083 <pedroazevedo83@hotmail.com>
2023-08-05 15:43:54 -0300

    Adicionar arquivos de projeto.

---
+++ TesteRebaseComVisualStudio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TesteRebaseComVisualStudio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
